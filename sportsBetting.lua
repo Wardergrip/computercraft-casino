3ec00b1c69edbeb7d47eec56505e8fd67f782ded
Wardergrip <42802496+Wardergrip@users.noreply.github.com>
2025-05-30 22:01:08 +0200

    Added creating prompts

--- sportsBetting.lua
+++ sportsBetting.lua
@@ -1,24 +1,53 @@
 os.loadAPI("cassapi.lua");
 
+PromptPath = "prompts.data";
 PromptPrefix = "@"
 UserPrefix = "#";
 
+LoadedCassPassInfo = nil;
+
 function GetPrompts()
-    local exists = fs.exists("prompts.data");
+    local exists = fs.exists(PromptPath);
     if exists == false or exists == nil then
         return nil;
     end
     local prompts = {};
-    local file = fs.open("prompts.data", "r");
+    local file = fs.open(PromptPath, "r");
     local line = file.readLine();
     while line ~= nil do
         local token = string.sub(line,1,1);
         if token == PromptPrefix then
-            prompts.insert(string.sub(line,2,string.len(line)));
+            table.insert(prompts, string.sub(line,2,string.len(line)));
         end
         line = file.readLine();
     end
     return prompts;
+end
+
+function CreatePrompt()
+    local answer = nil;
+    local question = nil;
+    while answer == nil or answer == false do
+        term.clear();
+        term.setTextColor(colors.yellow);
+        term.setCursorPos(1,1);
+        print("Ask a yes or no question that people will be able to bet on, you excluded.")
+        term.setTextColor(colors.white);
+        question = read();
+        if cassapi.IsNullOrEmpty(question) == false then
+            term.setTextColor(colors.yellow);
+            print("Confirm?")
+            answer = cassapi.GetUserYesNo();
+        else
+            term.setTextColor(colors.red);
+            print("Invalid question.")
+            sleep(1);
+        end
+    end
+    -- opens in write or append depending if it exists or not
+    local file = fs.open(PromptPath, fs.exists(PromptPath) and "a" or "w");
+    file.writeLine(PromptPrefix .. cassapi.GetCassPassName(LoadedCassPassInfo) .. PromptPrefix .. question);
+    file.close();
 end
 
 function LoopFunc()
@@ -35,8 +64,8 @@ function LoopFunc()
         sleep(5);
         return;
     end
-    local cassPassInfo = cassapi.ValidateCassPass();
-    if cassPassInfo == nil then
+    LoadedCassPassInfo = cassapi.ValidateCassPass();
+    if LoadedCassPassInfo == nil then
         disk.eject(diskDirection);
         term.clear();
         term.setTextColor(colors.red);
@@ -45,7 +74,7 @@ function LoopFunc()
         print("Invalid disk, not a valid casspass");
         sleep(5);
         return;
-    elseif cassPassInfo == false then
+    elseif LoadedCassPassInfo == false then
         disk.eject(diskDirection);
         term.clear();
         term.setTextColor(colors.red);
@@ -55,7 +84,22 @@ function LoopFunc()
         sleep(5);
         return;
     else
-        --local prompts = GetPrompts();
+        local prompts = GetPrompts();
+        if prompts == nil then
+            local answer = nil;
+            while answer == nil or answer == false do
+                term.clear();
+                term.setTextColor(colors.white);
+                term.setCursorPos(1,1);
+                print("No prompts to bet on.");
+                term.setTextColor(colors.yellow);
+                print("Create prompt?");
+                answer = cassapi.GetUserYesNo();
+            end
+            CreatePrompt();
+        else
+            Crash(); -- If I hit this line all my code works for now
+        end
     end
 end
 
